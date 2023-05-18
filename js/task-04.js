0a8a60a1bd0262ab153acf2cd58213c5a0f724f6
Jarylo <54155333+Jarylov@users.noreply.github.com>
2023-05-18 21:39:47 +0300

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,21 @@
+let counterValue = 0; 
+const valueSpan = document.getElementById("value");
+
+const updateCounterUI = () => {
+  valueSpan.textContent = counterValue;
+};
+
+const decrementButton = document.querySelector('button[data-action="decrement"]');
+const incrementButton = document.querySelector('button[data-action="increment"]');
+
+decrementButton.addEventListener("click", () => {
+  counterValue -= 1; 
+  updateCounterUI(); 
+});
+
+incrementButton.addEventListener("click", () => {
+  counterValue += 1; 
+  updateCounterUI();с
+});
+
+updateCounterUI();
