855daf95a7b94f1917c7268f65fe490553113e52
Jarylo <54155333+Jarylov@users.noreply.github.com>
2023-05-18 21:49:47 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
+const loginForm = document.querySelector('.login-form');
+const handleSubmit = (event) => {
+  event.preventDefault(); 
+
+  const { elements } = event.target;
+    
+  const isFormValid = [...elements].every((element) => {
+    return element.value.trim() !== ''; 
+  });
+
+  if (!isFormValid) {
+    alert('All fields must be filled in.');
+    return;
+  }
+
+  const formData = {};
+
+  [...elements].forEach((element) => {
+    if (element.name) {
+      formData[element.name] = element.value;
+    }
+  });
+
+  console.log(formData);
+  loginForm.reset();
+};
+
+loginForm.addEventListener('submit', handleSubmit);
 
