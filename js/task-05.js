186f6f1836d8dd553c0ea774589f5fc15e3d9db4
Jarylo <54155333+Jarylov@users.noreply.github.com>
2023-05-18 21:39:53 +0300

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+const nameInput = document.getElementById("name-input"); 
+const nameOutput = document.getElementById("name-output");
+
+const updateNameOutput = () => {
+  const inputText = nameInput.value; 
+
+  if (inputText.trim() !== "") {
+    nameOutput.textContent = inputText;
+  } else {
+    nameOutput.textContent = "Anonymous";
+  }
+};
+
+nameInput.addEventListener("input", updateNameOutput);
