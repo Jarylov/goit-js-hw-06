22a6c66bcc39f0bbf5b7c2cbed357f3e507230e8
Jarylo <54155333+Jarylov@users.noreply.github.com>
2023-05-19 19:48:07 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,39 @@
+const input = document.querySelector('#controls input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
+// Функція для генерації випадкового кольору
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 }
+
+// Функція для створення колекції елементів
+function createBoxes(amount) {
+  const boxes = [];
+
+  for (let i = 0; i < amount; i++) {
+    const size = 30 + i * 10;
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  }
+
+  boxesContainer.append(...boxes);
+}
+
+// Функція для очищення колекції елементів
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
+// Додаємо слухача події "click" до кнопки "Створити"
+createButton.addEventListener('click', () => {
+  const amount = Number(input.value);
+  createBoxes(amount);
+});
+
+// Додаємо слухача події "click" до кнопки "Очистити"
+destroyButton.addEventListener('click', destroyBoxes);
