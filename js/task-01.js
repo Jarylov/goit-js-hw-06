bab4d39f077ea7ded125192893d9452cd3a3e0d4
Jarylo <54155333+Jarylov@users.noreply.github.com>
2023-05-21 12:44:38 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -6,5 +6,6 @@ console.log(`Number of categories: ${categoriesItems.length}`);
 categoriesItems.forEach(item => {
   const categoryName = item.querySelector('h2').textContent;
   const categoryItemsCount = item.querySelectorAll('li').length;
-  console.log(`Category: ${categoryName} (items count: ${categoryItemsCount})`);
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${categoryItemsCount}`);
 });
